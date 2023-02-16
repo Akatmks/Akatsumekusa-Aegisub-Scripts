9c5df9a194d3b923300ccbe3f3a1f78009fba254
Akatsumekusa <Akatsumekusa@protonmail.com>
2023-02-16 14:39:51 +0000

    DepCtrl: Local post-merge update

--- modules/aka/workflow/init.lua
+++ modules/aka/workflow/init.lua
@@ -29,7 +29,7 @@ local versioning = {}
 
 versioning.name = "aka.workflow"
 versioning.description = "Module aka.workflow"
-versioning.version = "0.1.7"
+versioning.version = "0.1.8"
 versioning.author = "Akatsumekusa and contributors"
 versioning.namespace = "aka.workflow"
 
