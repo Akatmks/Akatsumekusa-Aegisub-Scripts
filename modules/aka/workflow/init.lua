1272671cd83fee7c97aad4262b0a5640fe089e20
Akatsumekusa <Akatsumekusa@protonmail.com>
2023-03-02 18:17:04 +0000

    nontomaticay: Update commit text

--- modules/aka/workflow/init.lua
+++ modules/aka/workflow/init.lua
@@ -29,7 +29,7 @@ local versioning = {}
 
 versioning.name = "aka.workflow"
 versioning.description = "Module aka.workflow"
-versioning.version = "0.1.16"
+versioning.version = "0.1.17"
 versioning.author = "Akatsumekusa and contributors"
 versioning.namespace = "aka.workflow"
 
--- modules/aka/workflow/run.lua
+++ modules/aka/workflow/run.lua
@@ -26,8 +26,6 @@ local config
 
 local run
 
-run = 
-
 
 
 
