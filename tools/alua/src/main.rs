aae3068e60ee7b983b15f21fc74fa7fa277e7512
Akatsumekusa <Akatsumekusa@protonmail.com>
2023-04-14 17:41:40 +0000

    alua: Finish the cli of the program

--- tools/alua/src/main.rs
+++ tools/alua/src/main.rs
@@ -1,5 +1,30 @@
+// alua
+// Copyright (c) Akatsumekusa and contributors
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+use alua;
 use clap::Parser;
+use std::ffi::OsStr;
+use std::fs;
 use std::path::PathBuf;
+use std::process;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,7 +37,43 @@ struct Cli {
 }
 
 fn main() {
-    let cli = Cli::parse();
-    println!("file: {:?}", cli.file);
-    println!("output: {:?}", cli.output);
+    let mut cli = Cli::parse();
+    cli.output = match cli.output {
+        Some(path) => Some(path),
+        None => {
+            let mut p = cli.file.clone();
+            if let Some("lua") = p.extension().and_then(OsStr::to_str) {
+                eprintln!("\x1b[31;1m[alua]\x1b[0m Input file has extension '\x1b[33m.lua\x1b[0m' but no output file is specified.");
+                eprintln!("\x1b[32;1m[alua]\x1b[0m If you want alua to overwrite input file, you need to explicitly specify it with the '\x1b[32m--output\x1b[0m' option.");
+                process::exit(1);
+            }
+            p.set_extension("lua");
+            Some(p)
+        }
+    };
+
+    let content = match fs::read_to_string(&cli.file) {
+        Ok(content) => content,
+        Err(msg) => {
+            eprintln!("\x1b[31;1m[alua]\x1b[0m Failed to read from the input file '\x1b[33m{}\x1b[0m':", cli.file.to_string_lossy());
+            eprintln!("\x1b[31;1m[alua]\x1b[0m {}.", msg);
+            process::exit(1);
+        }
+    };
+
+    let content = match alua::process(&content) {
+        Ok(content) => content,
+        Err(msgs) => {
+            for msg in &msgs {
+                eprintln!("\x1b[31;1m[alua]\x1b[0m {}", msg);
+            }
+            process::exit(1);
+        }
+    };
+
+    if let Err(msg) = fs::write(&cli.output.as_ref().unwrap(), content.as_ref()) {
+        eprintln!("\x1b[31;1m[alua]\x1b[0m Failed to write to the output file '\x1b[33m{}\x1b[0m':", cli.output.as_ref().unwrap().to_string_lossy());
+        eprintln!("\x1b[31;1m[alua]\x1b[0m {}.", msg);
+        process::exit(1);
+    }
 }
